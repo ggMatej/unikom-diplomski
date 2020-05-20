09f4bbec72092e1f40448478302cbc07299c29d6
Matej Mijić <ggm4tej@gmail.com>
2020-05-20 17:16:28 +0200

    Added config files

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-/**
- * @format
- */
+import React from 'react';
+import { AppRegistry, YellowBox, Text, View } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+YellowBox.ignoreWarnings(['Remote debugger']);
+
+const App = () => {
+    return (
+        <View>
+            <Text>Yoyo</Text>
+        </View>
+    );
+};
 
 AppRegistry.registerComponent(appName, () => App);
