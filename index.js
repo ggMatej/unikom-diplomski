e5f82ad7b1a6f5dd383ab710a974eeab6b325dc7
Matej Mijić <ggm4tej@gmail.com>
2020-05-26 17:50:34 +0200

    Removed plugin import/errors

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { AppRegistry, YellowBox, Text, View } from 'react-native';
+import { AppRegistry, YellowBox, View } from 'react-native';
 
 import { name as appName } from './app.json';
 
 YellowBox.ignoreWarnings(['Remote debugger']);
 
 const App = () => {
-    return (
-        <View>
-            <Text>Yoyo</Text>
-        </View>
-    );
+    return <View />;
 };
 
 AppRegistry.registerComponent(appName, () => App);
