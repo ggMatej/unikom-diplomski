10d617fa5c96db26958492335b460d7b153ce83d
ggMatej <ggm4tej@gmail.com>
2020-08-15 17:46:39 +0200

    Finished screen backgrounds

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,16 +1,48 @@
 import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, View, Dimensions } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Color } from 'global-styles';
+
+const screenWidth = Math.round(Dimensions.get('window').width);
+const screenHeight = Math.round(Dimensions.get('window').height);
 
 export const Home: React.FC = ({}) => {
   return (
     <SafeAreaView>
       <StatusBar backgroundColor={Color.Text} />
-      <View>
-        <Text>Home screen</Text>
+      <View style={styles.mainContainer}>
+        <View style={styles.topContainerBack}>
+          <View style={styles.topContainerFront} />
+        </View>
+        <View style={styles.bottomContainerBack}>
+          <View style={styles.bottomContainerFront} />
+        </View>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: '100%',
+    backgroundColor: Color.Secondary,
+  },
+  topContainerBack: {
+    flex: 7,
+    backgroundColor: Color.Primary,
+  },
+  topContainerFront: {
+    height: '100%',
+    backgroundColor: Color.Background,
+    borderBottomRightRadius: 55,
+  },
+  bottomContainerBack: {
+    flex: 1,
+    backgroundColor: Color.Background,
+  },
+  bottomContainerFront: {
+    height: '100%',
+    backgroundColor: Color.Primary,
+  },
+});
--- src/views/Reports.tsx
+++ src/views/Reports.tsx
@@ -1,16 +1,49 @@
 import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar, Dimensions, View, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { Color } from 'global-styles';
+
+const screenWidth = Math.round(Dimensions.get('window').width);
+const screenHeight = Math.round(Dimensions.get('window').height);
 
 export const Reports: React.FC = ({}) => {
   return (
     <SafeAreaView>
       <StatusBar backgroundColor={Color.Primary} />
-      <View>
-        <Text>Reports screen</Text>
+      <View style={styles.mainContainer}>
+        <View style={styles.topContainerBack}>
+          <View style={styles.topContainerFront} />
+        </View>
+        <View style={styles.bottomContainerBack}>
+          <View style={styles.bottomContainerFront} />
+        </View>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: '100%',
+    backgroundColor: Color.Secondary,
+  },
+  topContainerBack: {
+    flex: 1,
+    backgroundColor: Color.Primary,
+  },
+  topContainerFront: {
+    height: '100%',
+    backgroundColor: Color.Background,
+    borderBottomRightRadius: 150,
+  },
+  bottomContainerBack: {
+    flex: 1,
+    backgroundColor: Color.Background,
+  },
+  bottomContainerFront: {
+    height: '100%',
+    backgroundColor: Color.Primary,
+    borderTopLeftRadius: 150,
+  },
+});
