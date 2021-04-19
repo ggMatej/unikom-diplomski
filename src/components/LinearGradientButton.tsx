3b3c15d340ceb85ef3ffe5a3d59c6c4d4900878c
matejmijic <matej@prototyp.digital>
2021-04-19 17:08:12 +0200

    Removed import

--- src/components/LinearGradientButton.tsx
+++ src/components/LinearGradientButton.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { Color } from 'global-styles';
 
